292e833b759d05610d36782f4dc5a4b9b9f539c1
lwshakib <leadwithshakib@gmail.com>
2025-09-06 10:44:18 +0600

    Add Prisma setup with initial schema and migrations for user, message, conversation, and notifications models. Update package dependencies for Prisma integration and configure Vite server proxy for API requests.

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL || "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
+});
