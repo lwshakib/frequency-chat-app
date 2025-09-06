fddb8cf434e91609cfe7c58d67238307abfdbfb3
lwshakib <leadwithshakib@gmail.com>
2025-09-06 18:13:23 +0600

    Add SocketProvider component for WebSocket management
    - Introduced SocketContext for managing WebSocket connections.
    - Implemented state management for the socket within the SocketProvider.
    - Set up a basic structure for the SocketProvider component to wrap children components.

--- web-app/src/contexts/SocketProvider.tsx
+++ web-app/src/contexts/SocketProvider.tsx
@@ -1,3 +1,16 @@
-export const SocketProvider = ({children}: {})=>{
+import type React from "react";
+import { useEffect, useState } from "react";
 
+export const SocketContext = React.createContext(null)
+
+export const SocketProvider = ({children}: {children: React.ReactNode})=>{
+    const [socket, setSocket] = useState();
+
+    useEffect(()=>{
+        
+    })
+    return(
+    <SocketContext.Provider value={{socket}}>
+        {children}
+    </SocketContext.Provider>)
 }
