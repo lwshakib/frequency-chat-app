542a68a79bbff20ac3165e4317ee8f2301b86a36
lwshakib <leadwithshakib@gmail.com>
2025-09-06 11:06:02 +0600

    Add routing and authentication components
    - Integrated routing using react-router for navigation within the application.
    - Implemented authentication flow with Clerk, including SignedIn and SignedOut states.
    - Created a new ChatPage component for the main chat interface.
    - Updated package.json and package-lock.json to include new dependencies for react-router and @clerk/themes.
    - Refactored the main application file to include the BrowserRouter and Toaster components for enhanced user experience.

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -4,7 +4,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
+import { BrowserRouter } from "react-router";
+import { Toaster } from "@/components/ui/sonner";
+import {shadcn} from "@clerk/themes"
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -12,12 +14,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} appearance={shadcn}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <App />
       </ThemeProvider>
+      <Toaster />
     </ClerkProvider>
+    </BrowserRouter>
   </StrictMode>
 );
